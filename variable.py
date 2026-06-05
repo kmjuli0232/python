83ab2b91f0722125414a9d00a206757bac8c8b10
kmjuli0232 <kumarijuli7018@gmail.com>
2026-06-05 18:04:46 +0530

    python code

---
+++ variable.py
@@ -0,0 +1,14 @@
+name = "juli"
+age = 23
+price = 34.77
+
+print(name,age)
+
+print("my name is:",name)
+print("my age is:",age)
+
+# typeof
+
+print(type(name))
+print(type(age))
+print(type(price))

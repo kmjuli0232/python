2b443aada40c9f7f2f514b0b7db15c3261c0a4c3
kmjuli0232 <kumarijuli7018@gmail.com>
2026-06-08 16:10:35 +0530

    type of conversion

---
+++ conversion.py
@@ -0,0 +1,22 @@
+#🐳Type conversion  it convert automatically by python
+b = 3
+c = 4.25
+Sum = b+c
+print(type(Sum))
+
+#🐳type casting (manually)
+z = "2"
+y = 6.34
+x = int(z)
+sum = x+y
+
+print(sum)
+
+# convert to str
+
+g = 3.14
+g = str(g)
+ 
+print(type(g))
+
+

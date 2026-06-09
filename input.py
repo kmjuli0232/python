52a2d53b38ecfb379f896af5e2e32bc4b97f2e4a
kmjuli0232 <kumarijuli7018@gmail.com>
2026-06-09 13:55:31 +0530

    input in py

---
+++ input.py
@@ -0,0 +1,33 @@
+# print()# accept value by user
+
+# print()# result for input() isalways in "STR"
+
+# int(input()) # convert into int
+# float(print())# convert into float
+
+# input("enter your name: ")
+
+# age = input("enter your age: ")
+# print("your age", age )
+
+# val = input("enter some vale:")
+# print(type(val),val)
+
+#👽 convert inti int
+# int("7")
+# val = int(input("enter your value:"))
+# print(type(val),val)
+
+
+name = input("enter your name :")
+age = int(input("enter your age :"))
+marks = float(input("enter marsk:"))
+
+print("welcome:",name)
+print(type(age),age)
+print("age =",age )
+print(type(marks),marks)
+
+
+
+

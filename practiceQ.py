a2790df5fee9e76578ad80b2540d7e270f02d7cb
kmjuli0232 <kumarijuli7018@gmail.com>
2026-06-11 22:30:30 +0530

    python practice Q

---
+++ practiceQ.py
@@ -0,0 +1,58 @@
+# 1 Write a program to input 2 num & print their sum
+
+# first = int(input("enter first:"))
+# second = int(input("enter second:"))
+
+# print("sum =",first + second)
+
+# 2 WAP to print side of asquare & print its area
+# side = float(input("sqare side area:"))
+# print("area:",side * side)
+
+# 3 WAP to input 2 floating point num & print their average
+
+# s = float(input("enter first number:"))
+# k = float(input("enter second number:"))
+ 
+# print("average:",(s+k)/2)
+
+# 4 WAP to input 2 int num a and b.
+# print true if a is greater than or equal to b. if not print false
+# a = int(input("enter first num:"))
+# b = int(input("enter second num:"))
+# print(a>=b)
+
+# give the input abd add 2 num
+# first = int(input("enter first num:"))
+# second = int(input("enter second num:"))
+
+# print("sum =", first + second)
+
+
+# side = int(input("enter squer:"))
+# print("area =",side*side)
+
+
+# age = int(input("Enter your age:"))
+# print("add 5 = ", age+5)
+
+# num = int(input("enter num1:"))
+# num2 = int(input("enter num2:"))
+
+# print("number =",(num+num2),(num-num2),(num*num2),(num/num2),(num**num2))
+# print("addition =",num+num2)
+# print("sub =",num-num2)
+# print("multi =" ,num*num2)
+# print("devide =",num/num2)
+# print("reminder =",num%num2)
+
+# temp = float(input("enter tempture:"))
+# f = (C × 9/5) + 32
+# print("Temperature in Fahrenheit",(temp*9/5)+32)
+
+name = input("enter your name:")
+age = int(input("enter your age:"))
+
+print("My name is", name, "and I am", age, "years old.")
+
+

c9519f6a6e459e546ca556d733ba90b5c0fb5ab9
kmjuli0232 <kumarijuli7018@gmail.com>
2026-06-12 22:31:25 +0530

    operators in python

---
+++ operators.py
@@ -0,0 +1,44 @@
+
+# 🐳Arithmetic operator
+a=3
+b=4
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b) # alwase floting output
+print(a%b)# reminder
+print(a**b) #a^b
+
+# 🐳Relational/ comparision operator 👽alwase returen true and false 
+
+c = 59
+h = 67
+
+print(c == h) # False
+print(c != h) # True
+print(c >= h) # false
+print(c > h) # false
+print(c <= h) # true
+print(c < h) # true
+
+
+# 🐳Assignment operator
+
+num = 20
+# num += 10 # num = num+10
+# num -= 10
+# num *= 10
+# num %= 10
+num **= 2
+print("num :", num)
+
+# 🐳logical opertor
+
+k = 59
+d = 39
+print(not(k>d))
+print(not(d==k))
+
+print("And operator : ", (k>d)and(d<k)) # both conditionsare "True " then retusn true
+
+print("OR operator:",(k==d)or(k>d)) # when once a condition is true return "True" otherwise "False" 
